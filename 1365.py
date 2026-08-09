70675b3b338000679fe9b83058c5fff5c8c34f1f
devesh-compsci <deveshchampa@gmail.com>
2026-08-09 22:00:56 +0530

    1365 solved

---
+++ 1365.py
@@ -0,0 +1,14 @@
+class Solution:
+    def smallerNumbersThanCurrent(self, nums: List[int]) -> List[int]:
+        
+        d={}
+        temp = sorted(nums)
+        for i,num in enumerate(temp):
+            if num not in d:
+                d[num] = i
+        ret = []
+
+        for i in nums:
+            ret.append(d[i])
+
+        return ret
