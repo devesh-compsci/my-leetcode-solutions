7d0af1f76c980ec8400740c70dc1cccee15a1160
devesh-compsci <deveshchampa@gmail.com>
2026-08-09 22:33:40 +0530

    good try

---
+++ 1266.py
@@ -0,0 +1,17 @@
+# Own Solution
+class Solution:
+    def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        
+        time = 0
+
+        for i in range(len(points)-1):
+            dx = abs(points[i+1][0] - points[i][0])
+            dy = abs(points[i+1][1] - points[i][1])
+
+            if dx >= dy:
+                time = time + dx
+            else:
+                time = time + dy
+
+        return time
+
